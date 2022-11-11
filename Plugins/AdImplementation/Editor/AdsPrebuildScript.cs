f3eeda56bda44b6d3e5063761f5c039678611cfd
Benjamin BOUFFIER <benjamin.bouffier@gmail.com>
2022-11-11 11:11:10 +0400

    added Pre build script to copy necessary files into project before builds

---
+++ Plugins/AdImplementation/Editor/AdsPrebuildScript.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Plugins.AdImplementation.Editor
+{
+    public class AdsPrebuildScript : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            Debug.Log("AdsPrebuildScript.OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+            
+            // Copy link.xml into project
+            AssetDatabase.CopyAsset( "Packages/com.binouze.adimplementation/Plugins/GoogleMobileAds/link.xml",
+                                     "Assets/GoogleMobileAds/link.xml" );
+
+            // Copy GoogleMobileAdsSKAdNetworkItems.xml into project
+            AssetDatabase.CopyAsset( "Packages/com.binouze.adimplementation/Plugins/GoogleMobileAds/Editor/GoogleMobileAdsSKAdNetworkItems.xml",
+                                     "Assets/GoogleMobileAds/Editor/GoogleMobileAdsSKAdNetworkItems.xml" );
+        }
+    }
+}

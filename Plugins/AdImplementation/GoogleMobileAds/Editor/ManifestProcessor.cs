084e6b9030918f9ebc9716544e111bae7c4094ef
Benjamin BOUFFIER <benjamin.bouffier@gmail.com>
2022-11-11 15:21:42 +0400

    fix prebuild androidManifest processing

--- Plugins/AdImplementation/GoogleMobileAds/Editor/ManifestProcessor.cs
+++ Plugins/AdImplementation/GoogleMobileAds/Editor/ManifestProcessor.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if UNITY_ANDROID || true
+#if UNITY_ANDROID
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;

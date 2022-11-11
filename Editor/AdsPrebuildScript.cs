8328a21c7a484bc630303a0e16877a5b5847c2d6
Benjamin BOUFFIER <benjamin.bouffier@gmail.com>
2022-11-11 11:41:54 +0400

    fixed pre build script errors

--- Editor/AdsPrebuildScript.cs
+++ Editor/AdsPrebuildScript.cs
@@ -3,7 +3,7 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-namespace Plugins.AdImplementation.Editor
+namespace AdImplementation.Editor
 {
     public class AdsPrebuildScript : IPreprocessBuildWithReport
     {

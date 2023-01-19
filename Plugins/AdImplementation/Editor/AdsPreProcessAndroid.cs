1f442201435f2fbc1d76a940f63f8a36a1fd3a4e
Benjamin BOUFFIER <benjamin.bouffier@gmail.com>
2023-01-19 15:51:46 +0400

    test

--- Plugins/AdImplementation/Editor/AdsPreProcessAndroid.cs
+++ Plugins/AdImplementation/Editor/AdsPreProcessAndroid.cs
@@ -30,8 +30,8 @@ namespace com.binouze
 
             // -- TODO1 ADD android:name="androidx.multidex.MultiDexApplication" INSIDE APPLICATION TAG
             // set application android:name attribute
-            //XNamespace androidnamespace = "android"; 
-            elemApplication.SetAttributeValue( /*androidnamespace+*/"android:name", "androidx.multidex.MultiDexApplicatio" );
+            XNamespace androidnamespace = "http://schemas.android.com/apk/res/android"; 
+            elemApplication.SetAttributeValue( androidnamespace+"name", "androidx.multidex.MultiDexApplicatio" );
 
 
             // -- ADMOB
